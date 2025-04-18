8d9816b6dd91210eab667d4f29cb92e6e8422c50
snowmanparty <benj.lucas07@gmail.com>
2025-04-18 18:01:06 -0400

    filter gets data from data pipeline

--- lib/widgets/fliter.dart
+++ lib/widgets/fliter.dart
@@ -1,7 +1,7 @@
-
-
 import 'package:argent/components/data_pipeline.dart';
 import 'package:argent/main.dart';
+import 'package:argent/components/debug.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -24,13 +24,44 @@ class _FilterWidgetState extends State<FilterWidget> {
   /// Holds the current month filter
   String? currentMonth;
 
+  /// Holds the date ranges available per what is in the database
+  /// 
+  /// { year1 : [month1, month2, ...],
+  /// 
+  ///   year2 : [month4, month5, ..] }
+  Map<String, dynamic> dataRange = {};
+
+  /// Holds the component information for debugging messages
+  CompInfo compInfo = CompInfo('Filter', 2);
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    compInfo.printout('Reloading filters');
+    dataRange = await widget.dataPipeline.getTotalDateRange();
+  }
+
+  /// Gathers all the years from the data range into a list
   List<DropdownMenuItem> getYearChoices() {
     List<DropdownMenuItem> choices = [];
+    dataRange.forEach((year, month) {
+      choices.add(DropdownMenuItem(value: year, child: Text(year)));
+    });
     return choices;
   }
 
+  /// Gathers all the months for a certain year into a list
   List<DropdownMenuItem> getMonthChoices() {
     List<DropdownMenuItem> choices = [];
+    if (currentYear != null && dataRange.containsKey(currentYear)) {
+      for (String month in dataRange[currentYear]) {
+        choices.add(DropdownMenuItem(value: month, child: Text(month)));
+      }
+    }
     return choices;
   }
 
